32751fbe17014b8cef389d9e456e60f2c1f91f3e
mozngige <exscett@gmail.com>
2024-01-20 18:37:02 +0300

    sorting bugs

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -1,33 +1,8 @@
-"use client"
+import React from 'react'
 
-import { useForm } from "react-hook-form";
-const Footer = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log("me");
-  console.log(errors);
-
+ const Footer = () => {
   return (
-    <Footer>
-      <h3>Interesting Stories | Updates | Guides</h3>
-      <p>
-        Subscribe to learn about new technology and updates. Join over 5000+
-        members community to stay up to date with latest news.
-      </p>
-
-      <form>
-        <input
-          type="email"
-          placeholder="email"
-          {...register("email", { required: true })}
-        />
-
-        <input type="submit" />
-      </form>
-    </Footer>
-  );
-};
+    <div>Footer</div>
+  )
+}
 export default Footer
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,8 @@ import { cx } from './utils';
 import './globals.css'
 import { Inter, Manrope } from "next/font/google";
 import Header from './components/Header';
-import { Footer } from './components/Footer';
+import Footer from './components/Footer';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
